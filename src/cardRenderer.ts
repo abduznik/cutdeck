d828764723eec88f07ccc2774ad152e17723f218
abduznik <85239936+abduznik@users.noreply.github.com>
2026-07-03 13:50:53 +0300

    Add PDF capture diagnostic logging

--- src/cardRenderer.ts
+++ src/cardRenderer.ts
@@ -247,6 +247,15 @@ export class CardRenderer {
     const inner = this.container.querySelector('.card-rotated-inner') as HTMLElement;
     if (inner) await waitForImages(Array.from(inner.querySelectorAll('img')));
 
+    console.log(
+      `[pdf-capture] container clientW=${this.container.clientWidth} clientH=${this.container.clientHeight} ` +
+      `innerHTML.length=${this.container.innerHTML.length} ` +
+      `inner clientW=${inner?.clientWidth} clientH=${inner?.clientHeight} ` +
+      `inner scrollW=${inner?.scrollWidth} scrollH=${inner?.scrollHeight} ` +
+      `options W=${this.options.cardWidthPx} H=${this.options.cardHeightPx} ` +
+      `overflow=${getComputedStyle(this.container).overflow}`
+    );
+
     const html2canvas = (await import('html2canvas')).default;
     const canvas = await html2canvas(this.container, {
       scale: SCALE,
